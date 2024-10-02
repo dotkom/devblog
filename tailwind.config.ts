02bcad49752cf504e808f03a307c7cd6eb568cc1
Henry Græsberg (Mac) <henrygr@stud.ntnu.no>
2024-10-02 19:08:54 +0200

    Add color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#D29BE3",
+        secondary: "#711F8A",
+        accent: "#B432DB",
       },
     },
   },
