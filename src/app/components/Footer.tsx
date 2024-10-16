89f7acc4b7364052bf14e7d5bfa3004f99a23396
tharyng <theary.ngu@gmail.com>
2024-10-16 19:33:06 +0200

    fiksa footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
+import Logo from "@/app/assets/onboarding_logo_rev_2.png";
 
 const Footer =()=>{
     return(
-    <footer className="flex flex-1 justify-between py-4 px-10 bg-primary text-text_secondary">
+    <div className="flex flex-1 justify-between py-4 px-10 bg-primary text-white">
         <div className="flex gap-3">
-            <Image src= alt={"Dotkom devlog logo"} height={75}/> 
+            <Image src={Logo} alt={"Dotkom devlog logo"} height={75}/>
             <Link href="/" className="text-3xl font-bold"> Dotkom <br /> Devblog</Link>
         </div>
-        <div className="flex flex-rowgap-10 items-center font-bold text-2xl">
-            <div>
-            <Link href={}>Hjem</Link>
+        <div className="flex flex-col justify-evenly flex-rowgap-10 items-center">
+            <div className="flex gap-12 text-3xl">
+            <Link href={"/"}>Hjem</Link>
             <Link href={"/posts"}>Innlegg</Link>
             <Link href={"/about"}>Om oss</Link>
             </div>
-            <div><p>Laget med <3 av dotkom newbies! </p></div>
+            <div className="text-1xl "><p>Laget med &lt;3 av dotkom newbies! </p></div>
         </div>
         <div>
             <p>
@@ -26,7 +27,7 @@ const Footer =()=>{
 
             </p>
         </div>
-    </footer>) 
+    </div>) 
 }
 export default Footer
 
