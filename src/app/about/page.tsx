6e4fbc23b6556c16865fea1cf262b17a2244d8af
Your Name <you@example.com>
2024-10-23 19:09:27 +0200

    footer styling 2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "../components/Navbar";
 
 const Page = () => {
     return (
         <>
+        <Navbar />
             <div className="flex flex-1">
                 <h1>About</h1>
             </div>
