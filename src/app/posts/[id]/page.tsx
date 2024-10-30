63f8ce2a9180ad35c8fdbb3a9bcdbbe026e219e7
Henry Græsberg (Mac) <henrygr@stud.ntnu.no>
2024-10-30 21:52:30 +0100

    Add placeholder query for posts/[id] page

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,8 +1,17 @@
 import { FC } from 'react'
-import Image, { StaticImageData } from 'next/image'
-//import { }
+//import Image, { StaticImageData } from 'next/image'
+import { neon } from "@neondatabase/serverless"
 
-interface PostContent {
+export const getContent = async (id: string) => {
+  const sql = neon(process.env.DATABASE_URL as string)
+
+  //Placeholder table query
+  const post = await sql`SELECT * FROM playing_with_neon WHERE id = ${id}` //Replace playing_with_neon with name of posts table (Probably gonna be posts)
+
+  return post[0]
+}
+
+/*interface PostContent {
   title: string
   hook?: string
   image?: {
@@ -12,15 +21,20 @@ interface PostContent {
   author: string
   date: string
   content: string
-}
+}*/
 
-const Post: FC<{params: Promise<{id: string}>}> = ({ params }) => {
-  //content = await 
+const Post: FC<{params: Promise<{id: string}>}> = async ({ params }) => {
+  const id = (await params).id
   
+  const content = await getContent(id)
+  
+  console.log(content)
+
   return (
-    <>
       <main>
-        <div className="grid grid-cols-2 grid-rows-2 h-[50vh] w-[100vw] overflow-hidden relative gap-4">
+        {content.name}<br />
+        {content.value}
+        {/*<div className="grid grid-cols-2 grid-rows-2 h-[50vh] w-[100vw] overflow-hidden relative gap-4">
           <Image src={image.src} alt={image.alt} height={0} width={0} sizes="100vw" className="col-start-1 col-end-3 w-[100vw] h-auto" />
           <div className="col-start-1 row-start-2 z-10 m-6 bg-[#BE8CFF] p-4 w-fit rounded-xl flex flex-col gap-3">
             <h1 className="text-4xl text-text_secondary font-black">{title}</h1>
@@ -34,9 +48,8 @@ const Post: FC<{params: Promise<{id: string}>}> = ({ params }) => {
             <h2 className="text-lg">Publisert: {date}</h2>
           </div>
           <p dangerouslySetInnerHTML={{__html: content}}></p>
-        </article>
+        </article>*/}
       </main>
-    </>
   )
 }
 
