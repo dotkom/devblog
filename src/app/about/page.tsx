80194b1a36713294307ef8cab0912d5de4f22839
Henry Græsberg (Mac) <henrygr@stud.ntnu.no>
2024-10-31 08:20:40 +0100

    8AM, backend works. Ladies and gentlemen,  we got posts

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,8 @@
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-
 const Page = () => {
     return (
-        <>
         <div className="flex flex-1">
             <h1>About</h1>
         </div>
-        </>
     )
 }
 
